65943ad0d16b724ecf9a0b697366b274722a5916
unknown <henry.m.weiss.ctr@nga.mil>
2020-05-02 19:12:35 -0400

    lots of string stuff

--- recon1.py
+++ recon1.py
@@ -5,9 +5,54 @@ import numpy as np
 from dateutil import parser
 import re
 #import pudb
+import pdb
 
 filex='../st1/expenses20.csv'
 filet='../st1/expenses20.txt'
+filec='expenses20t.txt'  #reconciled
+
+def prntstk(stk,file1,file2):
+   mx = open(file1,'r')
+   px = open(file2,'w')
+   linb = mx.readlines()  
+   mx.close()
+   for i in range(len(linb )):
+      k = stk[i]
+      x = k.find('a')
+      linc = linb[i]
+      lind = linc.rstrip()      
+      mat=re.match('\d{2}[/]\d{2}[/]\d{2}', lind) 
+      if mat is not None:                      
+            sss = re.findall('\".*?\"',lind)           
+            if sss is not None:                           
+               nnn =re.findall('\d*\.?\d+',lind)
+               if nnn is not None:
+                   stg = sss[0]  
+                   sth = stg.replace('"',' ')                                         
+                   linf =('%-10s %-50s %-10s' %(mat[0],sth,nnn[3]))
+                   #linf = mat[0]+'  '+stg+'  '+nnn[3]
+                   print('-->%s' %(linf))
+      else:
+         linf = lind      
+      if x > -1:
+         px.write(' '+linf+'\n')
+      else: 
+         px.write('.'+linf+'\n')   
+      #pdb.set_trace()
+   px.close()          
+   return
+   
+
+def loadstk(file1):
+   jx = open(file1,'r')
+   lines = jx.readlines()
+   jx.close()
+   ln = len(lines)
+   stk= []
+   for i in range(ln):
+      val = str(i)+'a'
+      stk.append(val)
+   return stk   
 
 def cleanit(filea,fileb): 
    fx = open(filea,'rb')
@@ -26,7 +71,7 @@ def cleanit(filea,fileb):
    gx.close()
    return lx
 
-def findit(amt,dd):
+def findit(amt,dd,stk):
    #print(' start of findit %s' %(amt))      
    try:      
       fx = open(filet,'r') 
@@ -34,7 +79,7 @@ def findit(amt,dd):
       fx.close()      
    except:
       print('  File %s not found ' %(filet))      
-      return -2     
+      return -2, stk    
    la = len(lines)     
    for i in range(la):         
       lina = lines[i]
@@ -44,18 +89,22 @@ def findit(amt,dd):
          match = re.findall('\d{2}/\d{2}/\d{2}', lina)        
          dx = datetime.strptime(match[0],"%m/%d/%y")                         
       if ((x1>-1) and (len(match)==1)):
-         #print('  got it ')                   
-         return 1
+         #print('  got it ') 
+         valx = str(i)+'a'
+         s1 = stk.index(valx)
+         stk[s1]= str(i)+'b'         
+         return 1, stk
    #print(' end of findit search')   
-   return 0
+   return 0,stk
     
 def recon1():
    print('   Start - reconciled accounts')   
    lx = cleanit(filex,filet)
    if (lx < 5):
-       print(' Error: %s not found' %(fielex))
+       print(' Error: %s not found' %(filex))
        return -1
-   fnam2='Accounts2020t.txt'  # reconciled 
+   fnam2='Accounts2020t.txt'  # reconciled
+   stk = loadstk(filet)   
    hx = open(fnam2,'w')
    hx.write('        Accounts reconciled 2020 \n\n')
    fnam1='Accounts2020s.txt'
@@ -76,7 +125,7 @@ def recon1():
          dd = datetime.strptime(date,"%m/%d/%y")       
          famt = float(amt)       
          #print('  going to finditx')         
-         x = findit(amt,dd)
+         x,stk = findit(amt,dd,stk)
          ct = ct+1
          if x > 0:
             hx.write('.  %s\n' %(linb))
@@ -90,11 +139,12 @@ def recon1():
          break      
    hx.close()        
    gx.close()
+   prntstk(stk,filet,filec)
    if errx==1: 
       print('  Error processing records %s ' %(filet))  
       return -2
    else:      
-      print('   Processed %d records' %(ct))   
+      print('   Processed %d records' %(ct))          
    return 0  
  
 
